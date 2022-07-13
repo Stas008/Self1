64f3ba934b560ae2d54f06ea03ca4b3ad1a30f70
StanislavDalimov <stas008@hotmail.com>
2022-07-13 15:56:46 +0400

    Основной Алгоритм работАет

--- Program.cs
+++ Program.cs
@@ -3,11 +3,23 @@ int m = int.Parse(Console.ReadLine());
 Console.WriteLine(m);
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine (n);
-
+string str;
 int SumTwoEllements (int x, int y)
 {
     int sum=x+y;
     return sum;
 }
 
+str=Console.ReadLine();
 Console.WriteLine (SumTwoEllements(m,n));
+Console.WriteLine (str.Length);
+string[] words = str.Split(' ',',','.','!','?', ';');
+
+for (int i = 0; i < words.GetLength(0); i++)
+{
+    if (words[i].Length<=3)
+    {
+        Console.WriteLine(words[i]);
+    }
+}
+Console.WriteLine (words.GetLength(0));
