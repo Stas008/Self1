6fa1f83b239774e2e37c6ea5186eb7ba764a399a
StanislavDalimov <stas008@hotmail.com>
2022-07-13 13:36:26 +0400

    Добавили считывание элемента

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Какая то новая задача//
+
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(m);
