675d0eff6e96e76439f6e7216d8da8e5689f3877
StanislavDalimov <stas008@hotmail.com>
2022-07-13 13:51:49 +0400

    Добавили основную процедуру

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿//Какая то новая задача//
-
 int m = int.Parse(Console.ReadLine());
 Console.WriteLine(m);
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine (n);
+
+int SumTwoEllements (int x, int y)
+{
+    int sum=x+y;
+    return sum;
+}
+
+Console.WriteLine (SumTwoEllements(m,n));
